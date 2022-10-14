aaef8b79c01e6a41815bff9dc939dab4a44f45b8
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2022-10-14 23:19:21 +0100

    Re-opened after a while. made random computer choice. added some more but too tired, needs tidied.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
 // Determine computer choice
+let computerSelection;
+let playerSelection;
+
 function getComputerChoice() {
-    let choice =
-    return Math.random();
+    let random = Math.floor(Math.random() * 2);
+    if (random === 0) {
+        computerSelection = "Rock"
+    }
+    if (random === 1) {
+        computerSelection = "Scissors"
+    }
+    if (random === 2) {
+        computerSelection = "Paper"
+    }
+    return computerSelection
 }
 
 // Prompt for player choice
 
+
 // Determine who wins
+for (let i = 0; i < 5; i++) {
+    
+}
 if (playerSelection === computerSelection) {
     return "draw"
 } else if (
