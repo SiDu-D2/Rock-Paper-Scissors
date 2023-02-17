4c9061194f3551161dbcde740d47ea126c53e563
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2023-02-17 17:48:40 +0000

    Redoing it all!

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
 
-// Determine computer choice
-let computerSelection;
-let playerSelection;
+let computerOptions = ["rock", "paper", "scissors"];
 
+// Get computer selection
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 2);
-    if (random === 0) {
-        computerSelection = "Rock"
-    }
-    if (random === 1) {
-        computerSelection = "Scissors"
-    }
-    if (random === 2) {
-        computerSelection = "Paper"
-    }
-    return computerSelection
+    let computerNumber = Math.floor(Math.random() * 3);
+    return computerOptions[computerNumber];
 }
 
-// Prompt for player choice
-
-
-// Determine who wins
-for (let i = 0; i < 5; i++) {
-    
-}
-if (playerSelection === computerSelection) {
-    return "draw"
-} else if (
-    (playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "rock")
-) {
-    return "you lose";
-} else {
-    return "you win";
+// Get user selection - need BUTTONS
+function getUserChoice() {
+ return;
 }
 
-// Make count for wins/losses
+// Play game - compare choices
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw";
+    }
+}
