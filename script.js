624fe71c56cabbbd1bcf4223b8ce8d4373a3cb63
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2023-11-24 15:38:28 +0000

    Reworked again from the start

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 
+//Define options for computer
 let computerOptions = ["rock", "paper", "scissors"];
+// Create variable for input into gameplay, connected to function
+let computerSelection = getComputerChoice();
 
-// Get computer selection
+// Calculate computer selection using random maths function
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3);
     return computerOptions[computerNumber];
 }
 
-// Get user selection - need BUTTONS
-function getUserChoice() {
- return;
-}
+// Get player selection - need BUTTONS
+function getPlayerChoice() {
+    let playerSelection = prompt("Rock, paper, scissors").toLowerCase();
+    return playerSelection;
+    }
+
+let playerScore = 0;
+let computerScore = 0;
+let result = null;
 
 // Play game - compare choices
-function playGame(playerSelection, computerSelection) {
+function round(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a draw";
+        result = "It's a draw";
     }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        result = "YOU WIN, ROCK BEATS SCISSORS";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "YOU WIN, PAPER BEATS ROCK";
+    }
+    else if (playerSelection === "scissors" && computerSelection ==="paper") {
+        result = "YOU WIN, SCISSORS BEATS PAPER";
+    }
+    else { result = "You Lose"
+    }
+    return result;
 }
+
+function game() {
+    let index = 0;
+    while (index < 5) {
+        console.log(round(getPlayerChoice(), getComputerChoice()))
+        index++;
+        switch (true) {
+            case result.indexOf("You win") !== -1:
+                playerScore++;
+                console.log(`playerScore: ${playerScore}`);
+                break;
+            case result.indexOf("You lose") !== -1:
+                computerScore++;
+                console.log(`computerScore: ${computerScore}`)
+                break;
+        }
+    }
+    console.log(playerScore > computerScore ? "You win!" : "You lose!");
+}     
