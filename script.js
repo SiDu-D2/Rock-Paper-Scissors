be20b6714a9383fbca035bae80dfe9459701e8c4
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2023-12-11 16:37:59 +0000

    Updated script.js
    This is for the initial stages of the OdinProject, without any bells or whistles and it satisfies the criteria.

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
-
-const element = document.getElementById("rock");
-element.addEventListener("click", function() {
-  document.getElementById("result").innerHTML = round();
-});
-
-//Define options for computer
-let computerOptions = ["rock", "paper", "scissors"];
-// Create variable for input into gameplay, connected to function
-let computerSelection = getComputerChoice();
-
-// Calculate computer selection using random maths function
 function getComputerChoice() {
-    let computerNumber = Math.floor(Math.random() * 3);
-    return computerOptions[computerNumber];
+    let computerChoiceArray = ["rock", "paper", "scissors"];
+    let computerChoiceNumber = Math.floor(Math.random() * 3);
+    return computerChoiceArray[computerChoiceNumber];
 }
 
-// Get player selection - need BUTTONS
-function getPlayerChoice() {
-    let playerSelection = prompt("rock, paper, scissors").toLowerCase();
-    return playerSelection;
+function game(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = prompt("Round " + (i + 1) + ": Rock, paper, scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
     }
+}
 
-let playerScore = 0;
-let computerScore = 0;
-let result = null;
-
-// Play game - compare choices
-function round(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        result = "It's a draw";
+        return "It's a draw! Choose again.";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection + "!";
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection + "!";
     }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        result = "YOU WIN, ROCK BEATS SCISSORS";
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        result = "YOU WIN, PAPER BEATS ROCK";
-    }
-    else if (playerSelection === "scissors" && computerSelection ==="paper") {
-        result = "YOU WIN, SCISSORS BEATS PAPER";
-    }
-    else { result = "You Lose"
-    }
-    return result;
 }
 
-function game() {
-    let index = 0;
-    while (index < 5) {
-        console.log(round(getPlayerChoice(), getComputerChoice()))
-        index++;
-        switch (true) {
-            case result.indexOf("You win") !== -1:
-                playerScore++;
-                console.log(`playerScore: ${playerScore}`);
-                break;
-            case result.indexOf("You lose") !== -1:
-                computerScore++;
-                console.log(`computerScore: ${computerScore}`)
-                break;
-        }
-    }
-    console.log(playerScore > computerScore ? "You win!" : "You lose!");
-}     
-console.log(round("scissors", "scissors"));
+// Start the game
+game(5);
+
+
